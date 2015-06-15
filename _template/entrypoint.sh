62fdecf30b72a244358dd1db5cb314312fa99559
David Chung <dchung@qoriolabs.com>
2015-06-14 20:23:35 -0600

    Templates

--- _template/entrypoint.sh
+++ _template/entrypoint.sh
@@ -4,26 +4,27 @@
 #
 # Template for Docker image ENTRYPOINT
 #
-export PATH=$PWD:$PATH
 
-: ${DASH_BINARY:=$(cat ./GET_DASH)}
+: ${DASH_CMD:=$(cat ./DASH_CMD)}  # Cmd to fork and execute
+: ${DASH_BINARY:=$(cat ./DASH_BINARY)} # Dash binary url
+: ${DASH_OPTIONS:=$(cat ./DASH_OPTIONS)}
+: ${DASH_AUTH_TOKEN:=$(cat ./DASH_AUTH_TOKEN)}
+
+: ${DASH_CONFIG_URL:=""}
+: ${DASH_DOMAIN:=""}
+: ${DASH_VERSION:=$(cat ./VERSION)}
+: ${DASH_SERVICE:=$(echo $0 | sed -e 's/.sh//g' -e 's/.\///g')}
+: ${DASH_PATH:=""}
+
 # If command line is UPGRADE RUN ....  or UPGRADE -option=... then pull down the latest build of valet.
 if [[ $1 == "UPGRADE" ]]; then
     shift;
      wget ${DASH_BINARY}
      chmod a+x dash
+     export PATH=$PWD:$PATH
 fi
 
-: ${DASH_OPTIONS:="-daemon -no_source_env -logtostderr"}
-: ${DASH_CONFIG_URL:=""}
-: ${DASH_AUTH_TOKEN:=""}
-: ${DASH_CMD:=$(cat ./DASH_CMD)}
-
 ##############################################################
-: ${DASH_DOMAIN:=""}
-: ${DASH_VERSION:=$(cat ./VERSION)}
-: ${DASH_SERVICE:=$(echo $0 | sed -e 's/.sh//g' -e 's/.\///g')}
-: ${DASH_PATH:=""}
 
 case $1 in
 RUN)
